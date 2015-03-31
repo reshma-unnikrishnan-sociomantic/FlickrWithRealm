1ba826eb056b433d421eb0265032ada462ee182d
Reshma Unnikrishnan <reshma.unnikrishnan@sociomantic.com>
2015-03-31 14:25:52 +0200

    Initial Commit

---
+++ pinterestWithRealm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pinterestWithRealm
+//
+//  Created by Reshma on 31/03/15.
+//  Copyright (c) 2015 ruvlmoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pinterestWithRealm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pinterestWithRealm
+//
+//  Created by Reshma on 31/03/15.
+//  Copyright (c) 2015 ruvlmoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
